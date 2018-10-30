e5e63669f4e3276cfd1d1f1b8f3bb4760ebb546e
tgadf <tgadfort@gmail.com>
2018-10-30 11:22:28 -0500

    Working first version

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'utils',
+  py_modules = ['utils'],
+  version = '0.0.1',
+  description = 'General Utilities',
+  long_description = open('README.md').read(),
+  author = 'Thomas Gadfort',
+  author_email = 'tgadfort@gmail.com',
+  license = "MIT",
+  url = 'https://github.com/tgadf/utils',
+  keywords = ['utilities'],
+  classifiers = [
+    'Development Status :: 3',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities'
+  ],
+  install_requires = ['numpy', 'pandas', 'matplotlib', 'python-dateutil']
+)
+ 
