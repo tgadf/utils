05a32a8003ded63cb2563072796d7819a96631be
tgadf <tgadfort@gmail.com>
2019-03-14 00:39:57 -0500

    New API stuff

---
+++ apiUtils.py
@@ -0,0 +1,23 @@
+
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.parse.urlencode({
+})
+
+try:
+    conn = http.client.HTTPSConnection('api.fantasydata.net')
+    conn.request("GET", "/v3/cfb/stats/{format}/GamesByDate/{date}?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
