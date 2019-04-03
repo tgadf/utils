ad68dd6f7191acf7d64e422bf7507bdc4f3372bd
tgadf <tgadfort@gmail.com>
2019-04-02 22:28:48 -0500

    Adding momma function

--- mathUtils.py
+++ mathUtils.py
@@ -16,3 +16,19 @@ def doCirclesIntersect(com1, rad1, com2, rad2):
         return False
     else: 
         return True
+    
+    
+def siground(x, sig=3):
+    if not isinstance(x, (float, int)):
+        print("Must pass a real number. You passed a {0}".format(type(x)))
+        return None
+    
+    retval = '%s' % float('%.{0}g'.format(sig) % x)
+    
+    try:
+        result = float(retval)
+    except:
+        print("Could not turn [{0}] into a float.".format(retval))
+        result = None
+    
+    return result
