4a7c52fe6a494449c48eeb6e2848cddf5a347608
tgadf <tgadfort@gmail.com>
2020-09-12 22:36:39 -0500

    List stuff

--- listUtils.py
+++ listUtils.py
@@ -2,6 +2,17 @@ def getFlatList(listval):
     flat_list = [item for sublist in listval for item in sublist]
     return flat_list
 
+def isIn(values, listval):
+    if not isinstance(listval, list):
+        listval = [listval]
+    if not isinstance(values, list):
+        values = [values]
+    known = False    
+    for val in values:
+        for lval in listval:
+            if lval.find(val) != -1:
+                known = True
+    return known
 
 def getIndex(listval, val):
     try:
